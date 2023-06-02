5557cac2efa64d79973a125a2eae86ab03f67390
Ahadu Girma <ahagy9934@gmail.com>
2023-06-02 10:49:16 +0300

    merge interval the overlap if they have overlap

---
+++ merge-interval-overlap.py
@@ -0,0 +1,14 @@
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda x: x[0])  # Sort intervals based on the start time
+        
+        merged_intervals = []
+        
+        for interval in intervals:
+            if not merged_intervals or merged_intervals[-1][1] < interval[0]:
+                merged_intervals.append(interval)
+            else:
+                merged_intervals[-1][1] = max(merged_intervals[-1][1], interval[1])
+        
+        return merged_intervals
