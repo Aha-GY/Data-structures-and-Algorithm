2c9378046de90c547112bba775af51deddbb7bff
Ahadu Girma <ahagy9934@gmail.com>
2023-06-29 01:21:27 +0300

    solved revers integer

---
+++ revers integer.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+      
+       return int(ans)
+        arr = list(str(x))
+        
+        if x < 0 :
+            x = str(x)
+            arr[1:len(arr)]=arr[:0:-1]
+            y=int(''.join(arr))
+        else :
+            x = str(x)
+            y= int(x[::-1])
+        if y <= -2**31 or y >= (2**31)-1:
+            return 0
+        else :
+            return y
