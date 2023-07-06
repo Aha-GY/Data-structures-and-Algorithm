da1d9ba6a47c1be64bc38b64155f038a8b3a7c79
Ahadu Girma <ahagy9934@gmail.com>
2023-07-06 14:39:18 +0300

    solved Subdomain Visit Count

---
+++ two pointer example.py
@@ -0,0 +1,21 @@
+num = [2,7,4,5,0,1]
+num[1:4].sort()
+print(num)
+num.extend
+
+# sorted(num[0:-2])
+# print(num)
+# start = 0 
+# end = len(num)-1
+# while start < end :
+#     if num[start] == 0 :
+#         num[start],num[end] = num[end],num[start]
+#         end -= 1
+#     start +=1
+# print(num)
+# count = 0 
+# for i in range(len(num)):
+#     if num[i] == 0:
+#         count +=1
+# sorted(num[:-count])
+# print(num)
