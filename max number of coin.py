78a6ec9c4acc6fd113956263bcc974bc2d72675b
Ahadu Girma <ahagy9934@gmail.com>
2023-07-06 14:55:33 +0300

    find the max number of coins

---
+++ max number of coin.py
@@ -0,0 +1,18 @@
+
+class Solution:
+    def maxCoins(self, piles: list[int]) -> int:
+       
+        piles.sort(reverse=True)
+        total = 0
+        n = len(piles)//3
+        i = 0
+
+        while n > 0:
+            if i % 2 == 1:
+                total += piles[i]
+                print(total)
+                n -= 1
+            i += 1
+        return total
+
+
