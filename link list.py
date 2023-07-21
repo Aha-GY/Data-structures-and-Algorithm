193ed8415d950661ae7dbb3306e02b25b9e02a97
Ahadu Girma <ahagy9934@gmail.com>
2023-07-21 15:21:59 +0300

    check if it's palindrom by link list

---
+++ link list.py
@@ -0,0 +1,27 @@
+# class Node:
+#     def __init__(self, data=None):
+#         self.data = data
+#         self.next = None 
+
+# node1 = Node(5)
+# node2 = Node(7)
+# node1.next = node2
+# cherant_node = node1
+# while cherant_node:
+#     print(cherant_node)
+#     cherant_node = cherant_node.next
+# # class Node:
+# #     def __init__(self, data=None):
+# #         self.data = data
+# #         self.next = None
+# # node1 = Node(10)
+# # node2 = Node(20)
+# # node2.next = node1
+# # new_node = Node(5)
+# # new_node.next = node2
+# # head = new_node
+# # current_node = head
+# # while current_node:
+# #     print(current_node.data)
+# #     current_node = current_node.next
+print(factorial(5))  # Output: 120 (5 * 4 * 3 * 2 * 1)
