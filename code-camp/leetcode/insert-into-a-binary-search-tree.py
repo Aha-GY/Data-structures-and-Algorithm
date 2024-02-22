76f8c90cd76f518a333270356e702267486a7ba8
Aha-GY <123984812+Aha-GY@users.noreply.github.com>
2024-02-22 18:07:57 +0300

    Add solution for Insert into a Binary Search Tree

---
+++ code-camp/leetcode/insert-into-a-binary-search-tree.py
@@ -0,0 +1,12 @@
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        
+        if root is None:
+            return TreeNode(val)
+        
+        elif val > root.val:   
+            root.right = self.insertIntoBST(root.right,val) 
+            return root
+        elif val < root.val:
+            root.left = self.insertIntoBST(root.left,val)
+            return root
